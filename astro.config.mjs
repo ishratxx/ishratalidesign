18b2531e1a92671fc00230c27e6bd002f595ef4e
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2025-04-28 19:38:40 -0400

    maybe the url is wrong??

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ishratxx.github.io',
+  site: 'https://ishratxx.github.io/ishratalidesign/',
   vite: {
     plugins: [tailwindcss()]
   }
