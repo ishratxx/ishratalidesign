4c438466edce7c19c21ec9e8ab2e5d5488e065ef
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2025-04-28 20:23:18 -0400

    update base URL and navigation links for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ishratxx.github.io',
+  base: 'ishratalidesign',
   vite: {
     plugins: [tailwindcss()]
   }
