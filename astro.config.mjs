bbe600d37d87c7ebf8fa93101356b3ea5315dce2
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2025-05-05 21:49:40 -0400

    take 101

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ishratxx.github.io',
-  base: 'ishratalidesign',
+  site: 'https://ishratxx.github.io/ishratalidesign',
   vite: {
     plugins: [tailwindcss()]
   }
