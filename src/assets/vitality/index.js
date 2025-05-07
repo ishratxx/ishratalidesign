cf1d31571b8b45f57806c36809faab5b3dc3f03b
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2025-05-06 23:13:12 -0400

    refactor: consolidate image imports for Vitality Living page

---
+++ src/assets/vitality/index.js
@@ -0,0 +1,6 @@
+export { default as VLMockup1 } from './VLMockup1.svg';
+export { default as VLMockup2 } from './VLMockup2.svg';
+export { default as VLMockup3 } from './VLMockup3.svg';
+export { default as VLMockup4 } from './VLMockup4.svg';
+export { default as VLMockup5 } from './VLMockup5.svg';
+export { default as VitalityIG } from './VitalityIG.svg';
