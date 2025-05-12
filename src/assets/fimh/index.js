54d4baaba7cf6c8f3bd68accc6cb6d06ef7e0f24
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2025-05-12 00:42:10 -0400

    feat: add FIMH assets and implement splash screen component

---
+++ src/assets/fimh/index.js
@@ -0,0 +1,8 @@
+export { default as Chapter1 } from './chapter1.gif';
+export { default as Chapter2 } from './chapter2.gif';
+export { default as Chapter3 } from './chapter3.gif';
+export { default as Chapter4 } from './chapter4.gif';
+export { default as FIMHcovermockup } from './FIMHcovermockup.svg';
+export { default as FIMHmockup2 } from './FIMHmockup2.svg';
+export { default as FIMHmockup3 } from './FIMHmockup3.svg';
+export { default as FIMHstickermockup } from './FIMHstickermockup.png';
