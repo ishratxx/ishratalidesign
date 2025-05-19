e2a7719b90637045f2589155cadc2226a8b6fe4d
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2025-05-19 18:17:17 -0400

    feat: add new images and update project links for Jahira and Vitality Living

--- src/assets/jahira/index.js
+++ src/assets/jahira/index.js
@@ -3,3 +3,5 @@ export { default as jahirabags } from './JahiraBags.png';
 export { default as jahiracards } from './JahiraCards.png';
 export { default as jahiramockup1 } from './JahiraMockup1.png';
 export { default as jahiramockup2 } from './JahiraMockup2.png';
+export { default as BrandGuidelines } from './BrandGuidelines.svg';
+export { default as jahiraScreenMockups } from './JahiraScreenMockup.png';
