ed4429bbf8d665e29ddaaacdf6533b630dc964e7
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2025-05-18 21:58:35 -0400

    feat: add new project pages for Jahira and Divergent, including image assets and project details

---
+++ src/assets/divergent/index.js
@@ -0,0 +1,5 @@
+export { default as threecovers } from './3covers-min.png';
+export { default as allegiantmockup } from './Allegiantmockup-min.png';
+export { default as divergentmockup } from './Divergentmockup-min.png';
+export { default as insurgent } from './Insurgent-min.png';
+export { default as openmockup } from './OpenMockup-min.png';
---
+++ src/assets/jahira/index.js
@@ -0,0 +1,5 @@
+export { default as jahiragif } from './Jahira.gif';
+export { default as jahirabags } from './JahiraBags.png';
+export { default as jahiracards } from './JahiraCards.png';
+export { default as jahiramockup1 } from './JahiraMockup1.png';
+export { default as jahiramockup2 } from './JahiraMockup2.png';
