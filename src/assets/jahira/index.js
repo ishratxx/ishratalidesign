b4f3b8c3e546a8a48c105a709e6e5a44f1065b19
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2025-06-09 21:27:12 -0400

    Alot here

--- src/assets/jahira/index.js
+++
@@ -1,7 +0,0 @@
-export { default as jahiragif } from './Jahira.gif';
-export { default as jahirabags } from './JahiraBags.png';
-export { default as jahiracards } from './JahiraCards.png';
-export { default as jahiramockup1 } from './JahiraMockup1.png';
-export { default as jahiramockup2 } from './JahiraMockup2.png';
-export { default as BrandGuidelines } from './BrandGuidelines.png';
-export { default as jahiraScreenMockups } from './JahiraScreenMockup.png';
