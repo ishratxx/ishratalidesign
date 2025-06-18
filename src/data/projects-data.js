efcfe1a84f79e39fc92ba3e89fcc1dbb7959a407
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2025-06-17 21:39:06 -0400

    Refactor project pages to use dynamic routing and consolidate project data; remove individual project files

---
+++ src/data/projects-data.js
@@ -0,0 +1,93 @@
+// Data for Projects gallery pages
+export default [
+    {
+    title: "Vitality Living",
+    description: "Living",
+    projectDetails: [
+      { label: "Project Type", value: "Editorial Design" },
+      { label: "Role", value: "Designer" },
+      { label: "Tools Used", value: "Adobe InDesign | Illustrator | PhotoShop" },
+      { label: "Year", value: "2025" },
+    ],
+    rightColumn: [
+      "Vitality Living is a conceptual modern wellness magazine dedicated to empowering readers to live healthier, more balanced lives—inside and out. Its latest issue, Glow from Within, is a celebration of radiant skin as a reflection of internal well-being. This special edition takes a holistic approach to skincare, recognizing that true glow begins beneath the surface—through the foods we eat, the thoughts we carry, and the ways we care for our bodies and minds.",
+      "Whether you’re navigating seasonal changes in your skin or looking to simplify your beauty routine, Glow from Within offers approachable advice that feels both calming and empowering. More than just a magazine, Vitality Living is a wellness companion and believes that wellness isn’t one-size-fits-all. This issue is a gentle reminder that glowing skin isn’t about perfection—it’s about balance, rest, nourishment, and self-acceptance. With its clean visuals and warm, encouraging tone, Vitality Living continues to be a space where wellness feels personal, inclusive, and deeply rooted in care.",
+    ],
+    images: [
+      { col: 12, src: "/ishratalidesign/images/vitality/VLM1-min.png", alt: "Mockup 1", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/vitality/VLM2-min.png", alt: "Mockup 2", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/vitality/VLM3-min.png", alt: "Mockup 4", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/vitality/VLM4-min.png", alt: "Mockup 3", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/vitality/Social.png", alt: "Mockup 5", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/vitality/VLMockup5.svg", alt: "Vitality IG", width: "100vw", height: "100%" }
+    ]
+  },
+{
+    title: "Jahira",
+    description: "Skincare",
+    projectDetails: [
+      { label: "Project Type", value: "Brand Identity & Package Design" },
+      { label: "Role", value: "Designer" },
+      { label: "Tools Used", value: "Adobe InDesign | Illustrator | PhotoShop" },
+      { label: "Year", value: "2025" },
+    ],
+    rightColumn: [
+      "Jahira is a conceptual skincare line rooted in confidence, self-expression, and bold beauty. This line believes that every individual deserves to feel confident in their own skin. Jahira is crafted with a passion for enhancing your radiance, using scientifically proven ingredients and innovative formulations to deliver promising results. It contains products that can target any skin concern such as aging, hyperpigmentation and psoriasis with high quality ingredients such as Niacinamide and Centella Asiatica Extract. ",
+      "Designed to stand out on any shelf, Jahira’s packaging features vivid, saturated colors that reflect the energy and individuality of its users. More than just skincare, Jahira represents a fearless approach to self-care—where skin health meets artful design. Each product is thoughtfully formulated and visually striking, merging effective ingredients with a bold aesthetic that invites users to embrace their routine with intention and personality.",
+    ],
+    images: [
+      { col: 12, src: "/ishratalidesign/images/jahira/JahiraMockup1.png", alt: "Mockup 1", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/jahira/JahiraMockup2.png", alt: "Mockup 2", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/jahira/JahiraCards.png", alt: "Mockup 4", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/jahira/JahiraBags.png", alt: "Mockup 3", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/jahira/BrandGuidelines.png", alt: "winter months", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/jahira/JahiraScreenMockup.png", alt: "winter months", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/jahira/Jahira.gif", alt: "winter months", width: "100vw", height: "100%" }
+    ]
+  },
+  {
+    title: "Divergent",
+    description: "Reimagined",
+    projectDetails: [
+      { label: "Project Type", value: "Book Design & Illustration" },
+      { label: "Role", value: "Illustrator" },
+      { label: "Tools Used", value: "Adobe InDesign | Illustrator | PhotoShop" },
+      { label: "Year", value: "2025" },
+    ],
+    rightColumn: [
+      "For my reinterpretation of the Divergent series book covers, I took an illustrative approach that re-imagines the dystopian world through a more symbolic lens. Rather than focusing on the original photographic visuals, I leaned into layered compositions and minimal color palettes to evoke the internal struggles and themes woven throughout the story. Each cover centers around a single, abstract emblem that subtly references the corresponding faction while also reflecting the protagonist’s personal evolution.",
+      "This project allowed me to blend my love for storytelling and illustration, with a strong emphasis on cohesive layout design and visual hierarchy. I designed the typography to feel both bold and restrained, letting the illustrations take center stage while maintaining clarity and legibility. Together, the covers function as a unified set, but each one stands on its own with a distinct mood and visual rhythm. This reimagining of the Divergent series was not only an exercise in visual reinterpretation but also a chance to explore how illustration can breathe new emotional depth into familiar narratives.",
+    ],
+    images: [
+      { col: 12, src: "/ishratalidesign/images/divergent/3covers.png", alt: "Mockup 1", width: 1200, height: 800 },
+      { col: 12, src: "/ishratalidesign/images/divergent/divergent.png", alt: "Mockup 2", width: 1200, height: 800 },
+      { col: 12, src: "/ishratalidesign/images/divergent/insurgent.png", alt: "Mockup 4", width: 1200, height: 800 },
+      { col: 12, src: "/ishratalidesign/images/divergent/allegiant.png", alt: "Mockup 3", width: 1200, height: 800 },
+      { col: 12, src: "/ishratalidesign/images/divergent/open-mockup.png", alt: "winter months", width: 1200, height: 800 },
+    ]
+  },
+  {
+    title: "Flowers in My Heart",
+    description: "my heart",
+    projectDetails: [
+      { label: "Project Type", value: "Book Design & Illustration" },
+      { label: "Role", value: "Illustrator" },
+      { label: "Tools Used", value: "Adobe InDesign | Illustrator | PhotoShop" },
+      { label: "Year", value: "2023–2024" },
+    ],
+    rightColumn: [
+      "Flowers in My Heart is an illustrated book that explores the theme of estrangement through handwritten letters. The book collects four personal letters written by people close to me, each reflecting on their own experiences with distance, disconnection, and the emotional complexities of drifting apart from someone once close. Rather than simply presenting these letters as text, the book transforms them into visual narratives—each one becoming a bold, expressive illustration that brings the emotions within the words to life.",
+      "What makes Flowers in My Heart intimate is the preservation of each letter’s original handwriting. The rawness and individuality of each person’s script adds a human layer to the storytelling, making the emotions feel even more tangible. By pairing visual art with vulnerable words, Flowers in My Heart invites readers to sit with the beauty and ache of what it means to grow apart, while finding softness and strength in the space that remains.",
+    ],
+    images: [
+      { col: 12, src: "/ishratalidesign/images/fimh/cover.svg", alt: "Mockup 1", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/fimh/mockup2.svg", alt: "Mockup 2", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/fimh/mockup3.png", alt: "Mockup 2", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/fimh/mockup1.svg", alt: "Mockup 4", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/fimh/chapter1.gif", alt: "winter months", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/fimh/chapter2.gif", alt: "longer route", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/fimh/chapter3.gif", alt: "lost sibling", width: "100vw", height: "100%" },
+      { col: 12, src: "/ishratalidesign/images/fimh/chapter4.gif", alt: "perfect victim", width: "100vw", height: "100%" }
+    ]
+  },
+];
